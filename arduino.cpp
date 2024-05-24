ada0ed4b030d3f95804e3bf0395c4397d48fa65d
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2024-05-24 17:49:29 +0200

    Update arduino.cpp

--- arduino.cpp
+++ arduino.cpp
@@ -2,10 +2,10 @@
 // Created by lilian on 30/04/2024.
 //
 #include <DHT.h>
-#include <DHT.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Keypad.h>
+#include <Servo.h>
 
 // Configuration des pins pour les capteurs DHT et PIR et de l'alarme
 const int pinDHT = 2; // Pin où le capteur DHT22 est connecté
@@ -13,8 +13,8 @@ const int pinPIR = 3; // Pin où le capteur de mouvement PIR est connecté
 const int pinAlarme = 7; // Pin où l'alarme est connectée
 
 // Configuration des pins pour le module RFID
-const int RST_PIN = 9; // Pin de réinitialisation du module RFID
-const int SS_PIN = 10; // Pin de sélection du module RFID
+const int RST_PIN = 8; // Pin de réinitialisation du module RFID
+const int SS_PIN = 9; // Pin de sélection du module RFID
 
 // Configuration du clavier matriciel 4x4 pour l'authentification
 const byte ligneClavier = 4; // Ligne du clavier matriciel
@@ -25,11 +25,11 @@ char touchesClavier[ligneClavier][colonneClavier] = {
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}
 };
-byte pinLignesClavier[ligneClavier] = {5, 6, 7, 8};    // Pins des lignes du clavier matriciel
-byte pinColonnesClavier[colonneClavier] = {11, 12, 13, A0};  // Pins des colonnes du clavier matriciel
+byte pinLignesClavier[ligneClavier] = {22, 23, 24, 25};    // Pins des lignes du clavier matriciel
+byte pinColonnesClavier[colonneClavier] = {26, 27, 28, 29};  // Pins des colonnes du clavier matriciel
 Keypad clavier = Keypad(makeKeymap(touchesClavier), pinLignesClavier, pinColonnesClavier, ligneClavier, colonneClavier);
 
-String codeUIDcarte = ""; // Code UID de la carte autorisée
+String codeUIDcarte = " D3 1C D0 24"; // Code UID de la carte autorisée
 String code4chiffre = "1234"; // Code à 4 chiffres pour l'authentification
 
 // Configuration des seuils de variation de température et d'humidité
@@ -43,44 +43,37 @@ bool alarmeActive = false; // La variable pour vérifier si l'alarme est activé
 bool accesLegitime = false; // La variable pour vérifier l'accès légitime au coffre
 
 // Création de l'instance du capteur DHT et du module RFID
-MFRF522 rfid(SS_PIN, RST_PIN);
+Servo servo;
+MFRC522 rfid(SS_PIN, RST_PIN);
 DHT dht(pinDHT, DHT22);
-IRrecv irrecv(pinIR);
-decode_results resultats;
 
 void setup() {
     Serial.begin(9600);
+    servo.write(70); 
     SPI.begin();
     rfid.PCD_Init();
     dht.begin();
-    irrecv.enableIRIn(); // Démarrer la réception IR
+    servo.attach(3); 
     pinMode(pinAlarme, OUTPUT);
     pinMode(pinPIR, INPUT);
 }
 
 void loop() {
-    delay(2000); // Délai entre les mesures
     if (authentificationRFID()) {
         Serial.println("RFID validé, veuillez entrer votre code PIN:");
         if (verificationPIN()) {
             envoyerAlerte("Accès autorisé!");
         }
-    } else {
-        envoyerAlerte("Accès non autorisé!");
-    }
-    surveillerCapteurs();
-    if (irrecv.decode(&resultats)) {
-        if (resultats.value == 0xFFA25D) { // Code de la télécommande IR
-            alarmeActive = false;
-            digitalWrite(pinAlarme, LOW); // Désactiver l'alarme
-            irrecv.resume(); // Recevoir la prochaine valeur
-        }
     }
 }
 
 bool authentificationRFID() {
-    if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
-        return false;
+
+    if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+      envoyerAlerte("Accès non autorisé");
+      return false;
+    }
+
 
     String ID = "";
     for (byte i = 0; i < rfid.uid.size; i++) {
@@ -109,12 +102,6 @@ void surveillerCapteurs() {
     float humidite = dht.readHumidity(); // Lire l'humidité
     float temperature = dht.readTemperature(); // Lire la température
 
-    // Vérifier si les valeurs lues sont valides
-    if (isnan(humidite) || isnan(temperature)) {
-        Serial.println("Erreur de lecture du capteur DHT");
-        return;
-    }
-
     envoyerDataAffichage(temperature, humidite);
 
     int mouvement = digitalRead(pinPIR); // Lire l'état du capteur de mouvement
@@ -139,9 +126,10 @@ void surveillerCapteurs() {
 }
 
 void envoyerDataAffichage(float temperature, float humidite) {
-    // TODO Implémentez la fonctionnalité d'envoi des données d'affichage ici.
+  Serial.println(temperature);
+  Serial.println(humidite);
 }
 
 void envoyerAlerte(String message) {
-    // TODO Implémentez la fonctionnalité d'envoi d'alerte ici.
+  Serial.println(message);
 }
