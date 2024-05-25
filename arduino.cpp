82b99a4143be3f828bd14a9c5df9fcb4d2a4699d
Lilian <liliandamien@live.fr>
2024-05-25 22:42:04 +0200

    ⚡️Amélioration du programme

--- arduino.cpp
+++ arduino.cpp
@@ -1,6 +1,9 @@
+<<<<<<< Updated upstream
 //
 // Created by lilian on 30/04/2024.
 //
+=======
+>>>>>>> Stashed changes
 #include <DHT.h>
 #include <SPI.h>
 #include <MFRC522.h>
@@ -20,10 +23,10 @@ const int SS_PIN = 9; // Pin de sélection du module RFID
 const byte ligneClavier = 4; // Ligne du clavier matriciel
 const byte colonneClavier = 4; // Colonne du clavier matriciel
 char touchesClavier[ligneClavier][colonneClavier] = {
-    {'1', '2', '3', 'A'},
-    {'4', '5', '6', 'B'},
-    {'7', '8', '9', 'C'},
-    {'*', '0', '#', 'D'}
+        {'1', '2', '3', 'A'},
+        {'4', '5', '6', 'B'},
+        {'7', '8', '9', 'C'},
+        {'*', '0', '#', 'D'}
 };
 byte pinLignesClavier[ligneClavier] = {22, 23, 24, 25};    // Pins des lignes du clavier matriciel
 byte pinColonnesClavier[colonneClavier] = {26, 27, 28, 29};  // Pins des colonnes du clavier matriciel
@@ -33,8 +36,8 @@ String codeUIDcarte = " D3 1C D0 24"; // Code UID de la carte autorisée
 String code4chiffre = "1234"; // Code à 4 chiffres pour l'authentification
 
 // Configuration des seuils de variation de température et d'humidité
-const int seuilVariationTemp = 5; // Seuil de variation de température (en degrés Celsius)
-const int seuilVariationHum = 10; // Seuil de variation d'humidité (en pourcentage)
+const int seuilVariationTemp = 15; // Seuil de variation de température (en degrés Celsius)
+const int seuilVariationHum = 15; // Seuil de variation d'humidité (en pourcentage)
 
 // Variables pour stocker les dernières mesures valides
 float derniereTemp = 0.0;
@@ -49,11 +52,19 @@ DHT dht(pinDHT, DHT22);
 
 void setup() {
     Serial.begin(9600);
+<<<<<<< Updated upstream
     servo.write(70); 
     SPI.begin();
     rfid.PCD_Init();
     dht.begin();
     servo.attach(3); 
+=======
+    servo.write(70);
+    SPI.begin();
+    rfid.PCD_Init();
+    dht.begin();
+    servo.attach(3);
+>>>>>>> Stashed changes
     pinMode(pinAlarme, OUTPUT);
     pinMode(pinPIR, INPUT);
 }
@@ -64,16 +75,28 @@ void loop() {
         if (verificationPIN()) {
             envoyerAlerte("Accès autorisé!");
         }
+<<<<<<< Updated upstream
+=======
+    } else {
+        envoyerAlerte("Accès non autorisé!");
+        surveillerCapteurs();
+>>>>>>> Stashed changes
     }
 }
 
 bool authentificationRFID() {
 
     if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+<<<<<<< Updated upstream
       envoyerAlerte("Accès non autorisé");
       return false;
     }
 
+=======
+        envoyerAlerte("Accès non autorisé");
+        return false;
+    }
+>>>>>>> Stashed changes
 
     String ID = "";
     for (byte i = 0; i < rfid.uid.size; i++) {
@@ -101,13 +124,18 @@ bool verificationPIN() {
 void surveillerCapteurs() {
     float humidite = dht.readHumidity(); // Lire l'humidité
     float temperature = dht.readTemperature(); // Lire la température
+<<<<<<< Updated upstream
 
     envoyerDataAffichage(temperature, humidite);
 
+=======
+>>>>>>> Stashed changes
     int mouvement = digitalRead(pinPIR); // Lire l'état du capteur de mouvement
 
+    envoyerDataAffichage(temperature, humidite, mouvement);
+
     // Vérifier les conditions d'alerte
-    if (!accesLegitime && (abs(temperature - derniereTemp) > seuilVariationTemp || abs(humidite - derniereHum) > seuilVariationHum || mouvement)) {
+    if (!accesLegitime && (abs(temperature - derniereTemp) > seuilVariationTemp || abs(humidite - derniereHum) > seuilVariationHum || mouvement == HIGH)) {
         alarmeActive = true;
         digitalWrite(pinAlarme, HIGH); // Activer l'alarme
         if (mouvement) {
@@ -119,12 +147,16 @@ void surveillerCapteurs() {
         if (abs(humidite - derniereHum) > seuilVariationHum) {
             envoyerAlerte("Changement brusque d'humidité détecté!");
         }
+    } else {
+        alarmeActive = false;
+        digitalWrite(pinAlarme, LOW); // Désactiver l'alarme
     }
 
     derniereTemp = temperature;
     derniereHum = humidite;
 }
 
+<<<<<<< Updated upstream
 void envoyerDataAffichage(float temperature, float humidite) {
   Serial.println(temperature);
   Serial.println(humidite);
@@ -133,3 +165,14 @@ void envoyerDataAffichage(float temperature, float humidite) {
 void envoyerAlerte(String message) {
   Serial.println(message);
 }
+=======
+void envoyerDataAffichage(float temperature, float humidite, int mouvement) {
+    Serial.println(temperature);
+    Serial.println(humidite);
+    Serial.println(mouvement);
+}
+
+void envoyerAlerte(String message) {
+    Serial.println(message);
+}
+>>>>>>> Stashed changes
